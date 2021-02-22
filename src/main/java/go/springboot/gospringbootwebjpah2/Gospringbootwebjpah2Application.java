cc8be7ef5a3eff436975b6f2d946bfbdcfceb513
glenn opdycke-hansen <glennoph@gmail.com>
2021-02-21 18:25:26 -0600

    initial

---
+++ src/main/java/go/springboot/gospringbootwebjpah2/Gospringbootwebjpah2Application.java
@@ -0,0 +1,13 @@
+package go.springboot.gospringbootwebjpah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gospringbootwebjpah2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gospringbootwebjpah2Application.class, args);
+	}
+
+}
---
+++ src/test/java/go/springboot/gospringbootwebjpah2/Gospringbootwebjpah2ApplicationTests.java
@@ -0,0 +1,13 @@
+package go.springboot.gospringbootwebjpah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gospringbootwebjpah2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
